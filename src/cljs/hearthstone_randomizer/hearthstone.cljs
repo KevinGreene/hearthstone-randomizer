d6b59f11f76437074801625ffdf74bea4e393a70
Kevin Greene <kevin@spantree.net>
2015-02-21 23:25:19 -0500

    Removed person specific variables out

--- src/cljs/hearthstone_randomizer/hearthstone.cljs
+++ src/cljs/hearthstone_randomizer/hearthstone.cljs
@@ -4,9 +4,9 @@
 
 (def styles ["Suggest a Card" "Opponent's Choice" "Random - Click Done" "Max 3 Cost" "Minimum 5 Cost" "Class Specific" "Neutral Only"])
 
-(defn format-characters [kevins rachels]
-  (println (str "Kevin's is " kevins))
-  (println (str "Rachel's is " rachels)))
+(defn format-characters [player1 player2]
+  (println (str "Player 1 is " player1))
+  (println (str "Player 2 is" player2)))
 
 (defn get-characters []
   (take 2 (shuffle characters)))
